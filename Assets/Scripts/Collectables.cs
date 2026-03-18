60d2ed06391cf5b2b66681f2f56fb24e1e171e87
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-18 16:00:51 -0500

    Ejercisios 2

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    [SerializeField] private int value = 1;
+
+    [SerializeField] private float timeToDespawn;
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+}
