6ac9d4bc644f25120a1b42403e2861fe909c0959
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-18 16:37:09 -0500

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,36 @@
+using System;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class Player : MonoBehaviour
 {
     public int score = 0;
     
-    [SerializeField] private float speed = 5f;   
+    [SerializeField] private float speed = 5f;
+
+    public InputSystem_Actions inputs;
+
+    [SerializeField] private Vector2 moveInput;
+
+    private void Awake()
+    {
+        inputs = new();
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+        inputs.Player.Move.performed += OnMovement;
+        inputs.Player.Move.canceled += OnMovement;
+    }
+
+    private void OnMovement(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
     void Start()
     {
         
